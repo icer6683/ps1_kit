07e1947ffe52e711f3668dccdeda5327f8ddc357
icer6683 <andrewlou135@gmail.com>
2022-01-29 00:48:01 -0500

    Logistic Regression Finished

--- PS1-P4.py
+++ PS1-P4.py
@@ -4,3 +4,13 @@ import numpy as np
 from logistic_regression import LogisticRegression
 
 # HINT: this is really all the imports you need
+
+X_train, y_train, X_test, y_test = utils.load_all_train_test_data(
+    "P4")
+
+subsets_X, subsets_y = utils.load_learning_curve_data("P4/Train-subsets")
+for i, X in enumerate(subsets_X):
+    y = subsets_y[i]
+    # Train on X and y
+    ten = LogisticRegression()
+    ten.logistic_loss
--- logistic_regression.py
+++ logistic_regression.py
@@ -3,9 +3,11 @@ import utils
 from scipy.optimize import minimize
 from scipy.special import logsumexp
 
+
 class LogisticRegression():
     """ Logistic regression
     """
+
     def __init__(self, lam):
         """ Construct a least square regression object
         Don't modify this
@@ -17,14 +19,14 @@ class LogisticRegression():
                 If lam > 0 (regularized logistic regression)
         """
         self.lam = lam
-    
+
     def logistic_loss(self, w, X, y):
         """ Compute the logistic loss
 
         Args: 
             w (numpy.array)
                 Weight vector
-            
+
             X (numpy.array)
                 Input feature matrix
 
@@ -33,7 +35,7 @@ class LogisticRegression():
 
         Returns:
             Logistic loss
-        
+
         """
         # Your solution goes here
         f = np.dot(w.T, X)
@@ -43,14 +45,14 @@ class LogisticRegression():
             margin[i] = np.dot(y[i], f)
             in_log[i] = 1 + np.exp(-margin[i])
         return np.mean(np.log(in_log))
-        
+
     def fit(self, X, y, w0=None):
         """ Learn the weight vector of the logistic regressor via 
         iterative optimization using scipy.optimize.minimize
 
         Args:
             X (numpy.array)
-                Input feature matrix with shape (N,d)
+                Input feature matrix with shape (N,d)z
             y (numpy.array)
                 Label vector with shape (N,)
             w0 (numpy.array) 
@@ -70,22 +72,22 @@ class LogisticRegression():
 
         # You should initialize w0 here
         if not w0:
-            w0 = np.zero(len(X[0]))
-            # Do something instead of return
-            scipy.optimize.minimize
-            return
+            w0 = np.zero(len(X[0])+1)
 
+        ones = np.ones((len(X), 1))
+        Xnew = np.hstack(X, ones)
         # Hint:
-        res = minimize(self.logistic_loss, w0, args=(X, y)) 
-        w  = res["x"]
-
+        res = minimize(self.logistic_loss, w0, args=(Xnew, y))
+        w = res.x[:len(X[0])+1]
+        self.weight = w
+        b = w[len(X[0])+1]
+        self.bias = b
         # The rest of your solution goes here
-        return
+        return (w, b)
 
-        
     def predict(self, X):
         """ Predict the label {+1, -1} of data points
-        
+
         Args:
             X (numpy.array)
                 Input feature matrix with shape (N, d)
@@ -93,9 +95,10 @@ class LogisticRegression():
 
         Returns:
             y (numpy.array)
-                Predicted label vector with shape (N,)
+                Predicted pylabel vector with shape (N,)
                 Each entry is in {+1, -1}
 
         """
         # Your solution goes here
-        return
+        weight = np.hstack(self.weight, self.bias)
+        return np.sign(np.dot(np.transpose(weight), utils.augment_bias(X)) > 0)
