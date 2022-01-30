d6d0db6d875cd8c17b16f9797307f2dc31765555
icer6683 <andrewlou135@gmail.com>
2022-01-30 18:43:12 -0500

    Fixed L2 regularization

--- PS1-P4.py
+++ PS1-P4.py
@@ -4,10 +4,8 @@ import numpy as np
 from logistic_regression import LogisticRegression
 
 # HINT: this is really all the imports you need
-
 X_train, y_train, X_test, y_test = utils.load_all_train_test_data(
     "P4")
-"""
 subsets_X, subsets_y = utils.load_learning_curve_data("P4/Train-subsets")
 weight = None
 train_errors = np.empty(len(subsets_X))
@@ -16,7 +14,7 @@ test_errors = np.empty(len(subsets_X))
 for i, X in enumerate(subsets_X):
     y = subsets_y[i]
     # Train on X and y
-    train = LogisticRegression()
+    train = LogisticRegression(0)
     train.fit(X, y)
     ytrain = train.predict(X)
     train_errors[i] = utils.classification_error(ytrain, y)
@@ -29,20 +27,24 @@ xaxis = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 plt.plot(xaxis, test_errors)
 plt.xlabel("Percent Training Data")
 plt.ylabel("Testing Data Classification Errors")
-plt.show()
 plt.savefig("testing_learning_curve.png")
+plt.show()
 
 plt.plot(xaxis, train_errors)
 plt.xlabel("Percent Training Data")
 plt.ylabel("Training Data Classification Errors")
-plt.show()
 plt.savefig("training_learning_curve.png")
-"""
+plt.show()
 
-regularizer_values = [10**-7, 10**-6, 10**-
-                      5, 10**-4, 10**-3, 10**-2, 10**-1, 1]
+regularizer_values = np.array([10**-7, 10**-6, 10**-
+                               5, 10**-4, 10**-3, 10**-2, 10**-1, 1])
+
 train_errors_reg = np.empty(len(regularizer_values))
 test_errors_reg = np.empty(len(regularizer_values))
+fold = utils.load_all_cross_validation_data("P4/Cross-validation")
+
+cross_error = np.empty((len(regularizer_values), 5))
+
 for j in range(len(regularizer_values)):
     train_reg = LogisticRegression(regularizer_values[j])
     train_reg.fit(X_train, y_train)
@@ -51,17 +53,47 @@ for j in range(len(regularizer_values)):
 
     ytest = train_reg.predict(X_test)
     test_errors_reg[j] = utils.classification_error(ytest, y_test)
+
+    for k in range(5):
+        test_cross, train_cross = utils.partition_cross_validation_fold(
+            fold, k)
+        train_reg_cross = LogisticRegression(regularizer_values[j])
+        train_reg_cross.fit(train_cross[0], train_cross[1])
+        ytest_cross = train_reg_cross.predict(test_cross[0])
+        cross_error[j][k] = utils.classification_error(
+            ytest_cross, test_cross[1])
+        print(k)
     print(j)
 
-xaxis = [-7, -6, -5, -4, -3, -2, -1, 1]
-plt.plot(xaxis, test_errors_reg)
-plt.xlabel("Percent Training Data")
+xaxis2 = [-7, -6, -5, -4, -3, -2, -1, 1]
+plt.plot(xaxis2, test_errors_reg)
+plt.xlabel("Log of Lambda")
 plt.ylabel("Testing Data Classification Errors")
 plt.savefig("testing_learning_curve_reg.png")
 plt.show()
 
-plt.plot(xaxis, train_errors_reg)
-plt.xlabel("Percent Training Data")
+plt.plot(xaxis2, train_errors_reg)
+plt.xlabel("Log of Lambda")
 plt.ylabel("Training Data Classification Errors")
 plt.savefig("training_learning_curve_reg.png")
 plt.show()
+
+avg_reg_error = np.empty(len(regularizer_values))
+for i in range(len(avg_reg_error)):
+    avg_reg_error = np.mean(cross_error, axis=1)
+cross_index = np.argmin(avg_reg_error)
+best_lambda = regularizer_values[cross_index]
+
+print(cross_error)
+print("Set of all cross-validation errors are as follows: ", avg_reg_error)
+print("The optimal lambda is: "+str(best_lambda))
+final = LogisticRegression(best_lambda)
+final.fit(X_train, y_train)
+yfinal_train = final.predict(X_train)
+yfinal_test = final.predict(X_test)
+final_train_error = utils.classification_error(yfinal_train, y_train)
+final_test_error = utils.classification_error(yfinal_test, y_test)
+print("The corresponding training error is: " +
+      str(final_train_error))
+print("The corresponding testing error is: " +
+      str(final_test_error))
--- logistic_regression.py
+++ logistic_regression.py
@@ -38,21 +38,16 @@ class LogisticRegression():
 
         """
         # Your solution goes here
-        if not self.lam:
-            margin = np.empty(len(y))
-            in_log = np.empty(len(y))
-            for i in range(len(y)):
-                f = np.dot(np.reshape(w, (1, w.size)), X[i])
-                margin[i] = np.dot(y[i], f)
-                in_log[i] = 1 + np.exp(-margin[i])
-            return np.mean(np.log(in_log))
-        term_one = np.empty(len(y))
-        for j in range(len(y)):
-            term_one[j] = (
-                (np.dot(np.reshape(w, (1, w.size)), X[j])) - y[j])**2
-        for k in range(len(X[0])):
-            term_two = np.sum(self.lam*(w[k]) ** 2)
-        return np.mean(np.sum(term_one)) + term_two
+        margin = np.empty(len(y))
+        in_log = np.empty(len(y))
+        regularizer = 0
+        for i in range(len(y)):
+            f = np.dot(w, X[i])
+            margin[i] = np.dot(y[i], f)
+            in_log[i] = 1 + np.exp(-margin[i])
+        for j in range(len(X[0])):
+            regularizer += w[j]**2
+        return np.mean(np.log2(in_log)) + self.lam*regularizer
 
     def fit(self, X, y, w0=None):
         """ Learn the weight vector of the logistic regressor via
@@ -82,13 +77,12 @@ class LogisticRegression():
         if not w0:
             w0 = np.zeros(len(X[0])+1)
 
-        # Hint:
-        res = minimize(self.logistic_loss, w0, args=(utils.augment_bias(X), y))
+        res = minimize(self.logistic_loss, w0,
+                       args=(utils.augment_bias(X), y))
         w = res.x[:len(X[0])]
         self.weight = w
         b = res.x[len(X[0])]
         self.bias = b
-        # The rest of your solution goes here
         return (w, b)
 
     def predict(self, X):
@@ -109,8 +103,8 @@ class LogisticRegression():
         weight = np.hstack((self.weight, self.bias))
         yhat = np.empty(len(X))
         for i in range(len(yhat)):
-            yhat[i] = np.sign(np.dot(np.reshape(
-                weight, (1, weight.size)), np.transpose(utils.augment_bias(X)[i])))
+            yhat[i] = np.sign(
+                np.dot(weight, np.transpose(utils.augment_bias(X)[i])))
             if yhat[i] == 0:
                 yhat[i] = 1
         return yhat
